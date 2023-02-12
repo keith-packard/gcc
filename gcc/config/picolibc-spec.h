7a19f1f4acd18a2b1cac81036c9ce0ee0bdd8f7c
Keith Packard <keithp@keithp.com>
2023-02-12 14:23:32 -0800

    Support picolibc targets
    Match *-picolibc-* and select picolibc as the default C library, plus
    continuing to use the newlib-based logic for other configuration
    items.
    
    Add custom spec file fragments for use with picolibc:
    
     * '--oslib='. Allows targets to insert an OS library after the C
       library in the LIB_PATH spec file fragment. This library maps a few
       POSIX APIs used by picolibc to underlying system capabilities.
    
     * '--crt0='. Allows targets to use an alternate crt0 in place of the
       usual one as provided by Picolibc.
    
     * '--printf='. Allows targets to customize the version of printf linked
       from the C library.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ gcc/config/picolibc-spec.h
@@ -0,0 +1,53 @@
+/* Configuration common to all targets running Picolibc.
+   Copyright (C) 2023 Free Software Foundation, Inc.
+
+   This file is part of GCC.
+
+   GCC is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 3, or (at your
+   option) any later version.
+
+   GCC is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   Under Section 7 of GPL version 3, you are granted additional
+   permissions described in the GCC Runtime Library Exception, version
+   3.1, as published by the Free Software Foundation.
+
+   You should have received a copy of the GNU General Public License and
+   a copy of the GCC Runtime Library Exception along with this program;
+   see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#define PICOLIBC_LD "picolibc.ld"
+
+/* Default to local-exec TLS model.  */
+#undef OS_CC1_SPEC
+#define OS_CC1_SPEC " %{!ftls-model=*:-ftls-model=local-exec}"
+
+/* Pass along a preprocessor definition when --printf is specified */
+#undef CPP_SPEC
+#define CPP_SPEC \
+  "%{-printf=*: -D_PICOLIBC_PRINTF='%*'}"
+
+/* Add picolibc.ld if it exists. Define vfprintf and vfscanf if --printf is set */
+#undef LINK_SPEC
+#define LINK_SPEC \
+  "%:if-exists-then-else(%:find-file(" PICOLIBC_LD ") %{!T:-T%:find-file(" PICOLIBC_LD ")})"	\
+  " %{-printf=*:--defsym=vfprintf=__%*_vfprintf --defsym=vfscanf=__%*_vfscanf}" \
+
+/*
+ * Place the C library, libgcc and any oslib in a link group to resolve
+ * interdependencies
+ */
+#undef  LIB_SPEC
+#define LIB_SPEC "--start-group -lc %{-oslib=*:-l%*} %(libgcc) --end-group"
+
+/* Select alternate crt0 version if --crt0 is specified */
+#undef  STARTFILE_SPEC
+#define STARTFILE_SPEC "%{-crt0=*:crt0-%*%O%s; :crt0%O%s}"
+
+#define EH_TABLES_CAN_BE_READ_ONLY 1
